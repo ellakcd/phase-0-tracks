cd3d7dd04c659a4c995de533aa15f91be0b1bfd9
Ella Dershowitz <ella.dershowitz@gmail.com>
2017-03-07 14:24:38 -0800

    release 1 - hampster basic questions

---
+++ ruby/hamsters.rb
@@ -0,0 +1,20 @@
+puts "what's the hampster's name?"
+name = gets.chomp
+
+puts "volume level from 1 to 10?"
+volume = gets.chomp
+
+puts "fur color?"
+color = gets.chomp
+
+puts "good candidate?"
+good_candidate = gets.chomp
+
+puts "age?"
+age = gets.chomp
+if age == ""
+	age = nil
+end
+
+
+
