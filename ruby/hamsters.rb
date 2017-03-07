8af5639f180b621182b716466b3633ed6a492502
Ella Dershowitz <ella.dershowitz@gmail.com>
2017-03-07 14:32:03 -0800

    release 2 - fix data types

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -2,16 +2,23 @@ puts "what's the hampster's name?"
 name = gets.chomp
 
 puts "volume level from 1 to 10?"
-volume = gets.chomp
+volume = gets.chomp.to_i
+
 
 puts "fur color?"
 color = gets.chomp
 
-puts "good candidate?"
+puts "good candidate? (yes / no)"
 good_candidate = gets.chomp
+if good_candidate == "yes"
+	good_candidate = true
+else
+	good_candidate = false
+end
+
 
 puts "age?"
-age = gets.chomp
+age = gets.chomp.to_i
 if age == ""
 	age = nil
 end
