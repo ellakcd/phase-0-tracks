d851b4422da6ad7825a34fc62d6fb8f081bb63fd
Ella Dershowitz <ella.dershowitz@gmail.com>
2017-03-13 15:25:44 -0700

    release 3 - include shout module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,15 +1,43 @@
-module Shout
+# module Shout
 
-	def self.yell_angrily(words)
+# 	def self.yell_angrily(words)
+# 		words + "!!!" + " :("
+# 	end
+
+# 	def self.yell_happily(words)
+# 		words + "!!!" + " :)"
+# 	end
+
+# end
+
+
+# puts Shout.yell_angrily("Hello")
+# puts Shout.yell_happily("Hello")
+
+
+module Shout
+	def yell_angrily(words)
 		words + "!!!" + " :("
 	end
 
-	def self.yell_happily(words)
+	def yell_happily(words)
 		words + "!!!" + " :)"
 	end
-
 end
 
+class Baby
+	include Shout
+end
 
-puts Shout.yell_angrily("Hello")
-puts Shout.yell_happily("Hello")
+class Coach
+	include Shout
+end
+
+baby = Baby.new
+puts baby.yell_angrily("goo goo ga ga")
+puts baby.yell_happily("goo goo ga ga")
+
+
+coach = Coach.new
+puts coach.yell_angrily("Do you realize we just lost?")
+puts coach.yell_happily("We won")
