b44d6c3afe47a17382217b914607537d4e64ab0b
Ella Dershowitz <ella.dershowitz@gmail.com>
2017-03-13 14:39:56 -0700

    release 1 - add states

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,6 +1,10 @@
 class Santa
 
-	def initialize
+	def initialize(gender, ethnicity)
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
 		puts "Initializing Santa instance..."
 	end
 
@@ -15,6 +19,14 @@ class Santa
 end
 
 
-santy = Santa.new
+santy = Santa.new("N/A", "N/A")
 santy.speak
 santy.eat_milk_and_cookies("oreo")
+
+santas = []
+genders = ["male", "female", "none of your business"]
+ethnicities = ["black", "white", "asian"]
+l = genders.length 
+l.times do |i|
+santas << Santa.new(genders[i], ethnicities[i])
+end
