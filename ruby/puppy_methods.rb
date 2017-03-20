982ba9d25f4ffca041afc42a6b8ed8a759991574
Ella Dershowitz <ella.dershowitz@gmail.com>
2017-03-20 11:17:15 -0700

    release 0 - add behavior to class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,8 +1,76 @@
-class Puppy
+# class Puppy
 
-  def initialize
-  	puts "Initializing new puppy instance..."
-  end
+#   def initialize
+#   	puts "Initializing new puppy instance..."
+#   end
+
+#   def fetch(toy)
+#     puts "I brought back the #{toy}!"
+#     toy
+#   end
+
+#   def speak(num)
+#   	num.times {puts "Woof!"}
+#   end
+
+#   def roll_over
+#   	puts "*rolls over*"
+#   end
+
+#   def dog_years(years)
+#   	years * 7 
+#   end
+
+#   def chase_tail(num) 
+#   	num.times {puts "*chases tail*"}
+#   end
+
+# end
+
+
+# poochie = Puppy.new
+# poochie.fetch("bone")
+# poochie.speak(4)
+# poochie.roll_over
+# poochie.dog_years(5)
+# poochie.chase_tail(3)
+
+
+# class Climber
+
+# 	def initialize
+# 		puts "Another new climber is born"
+# 	end
+
+# 	def belay(num)
+# 		puts "Pass the belay test after just #{num} try/ies!"
+# 	end
+
+# 	def climb(num)
+# 		puts "Climb you first 5.#{num}"
+# 	end
+
+# 	def stretch
+# 		puts "Shake out those arms"
+# 	end
+
+# end
+
+
+# climbers = []
+# 50.times {climbers << Climber.new}
+# climbers.each do |person| 
+# 	person.belay(1)
+# 	person.climb(6)
+# 	person.stretch
+# end
+
+
+
+
+
+
+class Puppy
 
   def fetch(toy)
     puts "I brought back the #{toy}!"
@@ -17,51 +85,52 @@ class Puppy
   	puts "*rolls over*"
   end
 
-  def dog_years(years)
-  	years * 7 
+  def dog_years(human_years)
+  	human_years * 7
   end
 
-  def chase_tail(num) 
-  	num.times {puts "*chases tail*"}
+  def play_dead
+  	puts "*dies*"
   end
 
 end
 
 
-poochie = Puppy.new
-poochie.fetch("bone")
-poochie.speak(4)
-poochie.roll_over
-poochie.dog_years(5)
-poochie.chase_tail(3)
+rocky = Puppy.new
+rocky.fetch("ball")
+rocky.speak(4)
+rocky.roll_over
+puts rocky.dog_years(5)
+rocky.play_dead
 
 
-class Climber
-
-	def initialize
-		puts "Another new climber is born"
-	end
-
-	def belay(num)
-		puts "Pass the belay test after just #{num} try/ies!"
-	end
-
-	def climb(num)
-		puts "Climb you first 5.#{num}"
-	end
-
-	def stretch
-		puts "Shake out those arms"
-	end
-
-end
 
 
-climbers = []
-50.times {climbers << Climber.new}
-climbers.each do |person| 
-	person.belay(1)
-	person.climb(6)
-	person.stretch
-end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
