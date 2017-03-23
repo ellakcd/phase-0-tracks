20b495b65f3d018900c0e08e6a86260762f48ce1
Ella Dershowitz <ella.dershowitz@gmail.com>
2017-03-23 15:35:34 -0700

    make tests run

--- ruby/word_game/tests_spec.rb
+++ ruby/word_game/tests_spec.rb
@@ -4,13 +4,23 @@ describe Guessing_game do
 	let(:word) {Guessing_game.new("hello")}
 
 	it "checks if the guessed letter is in the word" do
-		expect(word.check_guess("o")).to eq true
+		expect(word.check_guess("o")).to eq nil
 	end
 	
+	it "displays win message and ends game" do
+		expect(word.win).to eq nil
+	end
 
+	it "displays loss message and ends game" do
+		expect(word.lose).to eq nil
+	end
+
+	it "exits game" do
+		expect(word.game_over).to eq nil
+	end
 
 	it "displays the current state of the word" do
-		expect (word.pretty_version(["h", "*", "l", "l", "*"], 2, 3).to eq "h*ll* (you've made 4 guesses out of 7))"
+		expect (word.pretty_version(["h", "*", "l", "l", "*"], 2, 3)).to eq "h*ll* (you've made 4 guesses out of 7))"
 	end
 	
 end
