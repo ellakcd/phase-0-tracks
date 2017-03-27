7bd89e92a200b8969de75d838ec6a33b736e4014
Ella Dershowitz <ella.dershowitz@gmail.com>
2017-03-27 16:47:54 -0700

    release 5 - refactor and delete unnecesary parameters

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -20,14 +20,14 @@ class VirusPredictor
 
 #calls 2 methods 
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths
+    speed_of_spread
   end
 
   private
 
 #calculates numbers of deaths based on population density and pop size and prints a string w the results / returns nil
-  def predicted_deaths(population_density, population, state)
+  def predicted_deaths
     # predicted deaths is solely based on population density
     if @population_density >= 200
       number_of_deaths = (@population * 0.4).floor
@@ -46,7 +46,7 @@ class VirusPredictor
   end
 
 #calculats speed of spread based on population density and prints string / returns nil
-  def speed_of_spread(population_density, state) #in months
+  def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
