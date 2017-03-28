346da698c529e064beee370d923165281e45d967
Ella Dershowitz <ella.dershowitz@gmail.com>
2017-03-27 17:11:28 -0700

    lots of refactoring

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -18,53 +18,54 @@ class VirusPredictor
     @population_density = population_density
   end
 
+
+
 #calls 2 methods 
   def virus_effects
-    predicted_deaths
-    speed_of_spread
+    print "#{@state} will lose #{number_of_predicted_deaths} people in this outbreak"
+    puts " and will spread across the state in #{speed_of_spread} months.\n\n"
   end
 
   private
 
 #calculates numbers of deaths based on population density and pop size and prints a string w the results / returns nil
-  def predicted_deaths
-    # predicted deaths is solely based on population density
+def percent_calc  
+
     if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
+      0.4
     elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
+      0.3
     elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
+      0.2
     elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+      0.1
     else
-      number_of_deaths = (@population * 0.05).floor
+      0.05
     end
-
-    print "#{@state} will lose #{number_of_deaths} people in this outbreak"
-
   end
 
-#calculats speed of spread based on population density and prints string / returns nil
+  def number_of_predicted_deaths
+    # predicted deaths is solely based on population density
+
+    (@population * percent_calc).floor
+  end
+
+#calculates speed of spread based on population density and prints string / returns nil
   def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
-    speed = 0.0
 
     if @population_density >= 200
-      speed += 0.5
+      0.5
     elsif @population_density >= 150
-      speed += 1
+      1
     elsif @population_density >= 100
-      speed += 1.5
+      1.5
     elsif @population_density >= 50
-      speed += 2
+      2
     else
-      speed += 2.5
+      2.5
     end
-
-    puts " and will spread across the state in #{speed} months.\n\n"
-
   end
 
 end
