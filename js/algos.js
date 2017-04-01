fc6b3510a60e3dc31ecd7c0d012cc2a1aedf6fa5
Ella Dershowitz <ella.dershowitz@gmail.com>
2017-04-01 17:36:44 -0400

    create function to compare two objects by key and value

--- js/algos.js
+++ js/algos.js
@@ -15,8 +15,45 @@ return current_word;
 }
 
 
-test = longest_word(["long phrase","longest phrase","longer phrase"]);
+
+function object_match(obj1, obj2){
+	var keys1 = [];
+	var keys2 = [];
+	for (key in obj1){
+		keys1.push(key);
+	}
+	for (key in obj2){
+		keys2.push(key);
+	}
+	for (var i = 0; i < keys1.length; i ++){
+		var key = keys1[i];
+		if (keys2.indexOf(key) !== 0){
+			val1 = obj1[key];
+			val2 = obj2[key];
+			if (val1 === val2){
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+
+
+
+
+
+
+
+
+var test = longest_word(["long phrase","longest phrase","longer phrase"]);
 console.log(test);
 
-test_2 = longest_word(["1", "22", "666666", "4444", "333"]);
+var test_2 = longest_word(["1", "22", "666666", "4444", "333"]);
 console.log(test_2);
+
+var test_3 = object_match({name: "Steven", age: 54}, {name: "Tamir", age: 54});
+console.log(test_3);
+
+var test_4 = object_match({name: "Steven", age: 53}, {name: "Tamir", age: 54});
+console.log(test_4);
