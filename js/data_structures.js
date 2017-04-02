39371d6ce660bfed1fdf23f72202ea505b11eae8
Ella Dershowitz <ella.dershowitz@gmail.com>
2017-04-02 11:11:36 -0400

    created color and horse name array

---
+++ js/data_structures.js
@@ -0,0 +1,9 @@
+var colors_array = ["purple", "green", "brown", "white"];
+
+var names_array = ["Ed", "Tom", "Lucy", "Charlie"];
+
+colors_array.push("orange");
+names_array.push("Bill");
+
+console.log(colors_array);
+console.log(names_array);
