22d74de1095aad7f72be092d7570da08a9804fc3
Ella Dershowitz <ella.dershowitz@gmail.com>
2017-04-02 11:28:36 -0400

    create hash that pairs names with colors

--- js/data_structures.js
+++ js/data_structures.js
@@ -7,3 +7,21 @@ names_array.push("Bill");
 
 console.log(colors_array);
 console.log(names_array);
+
+
+
+
+var horses = {};
+
+if(colors_array.length === names_array.length){
+for (var i = 0; i < colors_array.length; i++){
+var name = names_array[i];
+var color = colors_array[i];
+horses[name] = color;
+    console.log(horses);
+}
+}
+else {
+	console.log("Can't be grouped");
+}
+
